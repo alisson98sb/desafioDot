89ebb1b93c9262ec27d342e5aca55a6bfc618b3a
alisson98sb <alisson98sb@gmail.com>
2023-01-06 14:06:11 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [list, setList] = useState([])
+  const [listRemoved, setListRemoved] = useState([])
+
+  function handleClick(event) {
+    if(list.length === 0 ) setListRemoved([]);
+  
+    const newDot = {
+      clientX : event.clientX,
+      clientY : event.clientY
+    }
+
+    setList((prev) => [...prev, newDot])
+  }
+  
+  function handleUndo(event) {
+    event.stopPropagation()
+
+
+    if(list.length === 0 ) return;
+
+    const lastDot = list[list.length - 1];
+    setListRemoved((prev)=> [...prev, lastDot])
+
+    setList((prev)=> {
+      const newArr = [...prev].slice(0, -1);
+      return newArr;
+    })
+  }
+
+  function handleReDo(event){
+    event.stopPropagation()
+
+    if(listRemoved.length === 0 ) return;
+
+    const lastDot = listRemoved[listRemoved.length - 1];
+    setList((prev)=> [...prev, lastDot])
+    
+    setListRemoved((prev) => {
+      const newArr = [...prev].slice(0, -1);
+      return newArr;
+    })
+  }
+
+  function handleReset(event) {
+    event.stopPropagation()
+    setListRemoved(list)
+    setList([])
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="Page" onClick={handleClick}>
+      {list.map((item) => (
+        <span id="dot" key={item.clientX} style={{left: item.clientX, top: item.clientY} }/>
+      ))}
+
+      <div id='dotButtons'>
+        <button onClick={handleUndo}>Desfazer</button>
+        <button onClick={handleReDo}>Refazer</button>
+        <button onClick={handleReset}>Zerar</button>
+      </div>
     </div>
   );
 }
